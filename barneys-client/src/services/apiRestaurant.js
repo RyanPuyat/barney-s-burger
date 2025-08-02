49074e8db71b88a7bb4cb47d7f96369712920342
Ryan <ryanspuyat@gmail.com>
2025-08-02 23:52:13 +0900

    changing server url

--- barneys-client/src/services/apiRestaurant.js
+++ barneys-client/src/services/apiRestaurant.js
@@ -1,5 +1,5 @@
 // const API_URL = 'http://localhost:5000/api';
-const API_URL = 'https://barneys-burger-server.onrender.com/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getMenu() {
   const res = await fetch(`${API_URL}/menu`);
